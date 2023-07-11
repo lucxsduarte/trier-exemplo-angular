716578b760cca9af98a1030cf0d6ad04c20dc471
Lucas Duarte <102632990+lucxsduarte@users.noreply.github.com>
2023-07-10 23:41:42 -0300

    microondas finalizado

--- src/app/microondas/microondas.component.ts
+++ src/app/microondas/microondas.component.ts
@@ -12,8 +12,10 @@ export class MicroondasComponent {
   public tempoAdicional: number = 0;
   public contador: any;
   public contadorAtivo: boolean = false;
+  public acrescentaTempo: boolean = false;
 
   public adicionaTempo(value: number) {
+    
     if (!this.contadorAtivo) {
       if (this.tempoVisor === 0) {
         this.tempoVisor = value;
@@ -49,17 +51,45 @@ export class MicroondasComponent {
   }
 
   private atualizarVisor() {
-    const minutos = Math.floor(this.tempoVisor / 60);
-    const segundos = this.tempoVisor % 60;
-    this.totalVisor = this.formatTime(minutos) + ":" + this.formatTime(segundos);
-  }
+    
+    let X = Math.floor(this.tempoVisor / 1000);
+    let Y = Math.floor((this.tempoVisor % 1000) / 100);
+    let Z = Math.floor((this.tempoVisor  % 100) / 10);
+    let W = this.tempoVisor  % 10;
+    
+    let zero = 0;
+    let minutos = X * 10 + Y;
+    let segundos = Z * 10 + W;
+    
+    if (segundos > 60 && segundos <= 99) {
+      let diferença = segundos - 60;
+      this.tempoVisor = this.tempoVisor - diferença;
+      segundos = 59;
+    } 
+      
+    if(this.tempoVisor > 60 && this.tempoVisor < 100){
+      segundos = 59;
+    }
 
+    if (minutos < 10 && segundos < 10) {
+      this.totalVisor = `${zero}${minutos}:${zero}${segundos}`;
+    } else if (minutos < 10) {
+      this.totalVisor = `${zero}${minutos}:${segundos}`;
+    } else if (segundos < 10) {
+      this.totalVisor = `${minutos}:${zero}${segundos}`;
+    } else {
+      this.totalVisor = `${minutos}:${segundos}`;
+    }
+
+  }
   formatTime(time: number): string {
     return time < 10 ? "0" + time : String(time);
   }
 
   public acrescentarTempo(tempo: number) {
+    this.acrescentaTempo = true;
     this.tempoVisor += tempo;
     this.atualizarVisor();
   }
+  
 }
